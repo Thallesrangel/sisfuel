5daf51abcb45a3f6a6d0b2dbdb2863a5280cac32
Thalles Rangel <ranglethr@gmail.com>
2020-06-18 00:20:08 -0300

    sql

--- sisfuel.sql
+++ sisfuel.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 12/06/2020 às 02:15
+-- Tempo de geração: 18/06/2020 às 05:19
 -- Versão do servidor: 10.4.11-MariaDB
 -- Versão do PHP: 7.4.6
 
@@ -65,8 +65,9 @@ CREATE TABLE `tbcartao_virtual` (
 
 INSERT INTO `tbcartao_virtual` (`id_cartao`, `id_cliente`, `id_usuario`, `valor_limite`, `data_validade`, `id_motorista`, `id_cartao_situacao`, `id_cartao_renovacao`, `flag_excluido`) VALUES
 (6, 12, 49, '200.00', '2020-06-24', 40, 1, 1, 1),
-(7, 11, 48, '123.00', '2020-07-02', 41, 1, 1, 0),
-(8, 11, 48, '123.00', '2020-06-25', 41, 1, 1, 0);
+(7, 11, 48, '123.00', '2020-07-02', 41, 1, 1, 1),
+(8, 11, 48, '123.00', '2020-06-25', 41, 1, 1, 1),
+(9, 11, 48, '1.00', '2020-06-17', 41, 1, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -81,7 +82,7 @@ CREATE TABLE `tbcartao_virtual_movimentos` (
   `id_fornecedor` int(11) NOT NULL,
   `id_veiculo` int(11) NOT NULL,
   `id_combustivel` int(11) NOT NULL,
-  `quantidade` decimal(10,1) NOT NULL,
+  `quantidade` decimal(10,2) NOT NULL,
   `valor_unitario` decimal(10,2) NOT NULL,
   `valor_total` decimal(10,2) NOT NULL,
   `id_cartao_movimento_situacao` int(11) NOT NULL
@@ -434,7 +435,7 @@ CREATE TABLE `tbmov_entrada` (
   `id_entrada` int(11) NOT NULL,
   `id_cliente` int(11) DEFAULT NULL,
   `nota_fiscal` int(50) DEFAULT NULL,
-  `quantidade` float NOT NULL,
+  `quantidade` double(10,2) NOT NULL,
   `id_fornecedor` int(11) NOT NULL,
   `id_tanque` int(11) NOT NULL,
   `motorista` varchar(60) NOT NULL,
@@ -450,13 +451,19 @@ CREATE TABLE `tbmov_entrada` (
 --
 
 INSERT INTO `tbmov_entrada` (`id_entrada`, `id_cliente`, `nota_fiscal`, `quantidade`, `id_fornecedor`, `id_tanque`, `motorista`, `data_entrada`, `placa`, `valor_unitario`, `valor_total`, `flag_excluido`) VALUES
-(183, 11, 30, 10000, 14, 40, 'Luan Roberto Ribeiro', '2020-06-09 06:15:00', 'PPS2314', '3.20', '32000.00', 0),
-(184, 11, 21321, 123, 14, 40, 'thalles', '2020-06-09 11:04:00', 'PPS1232', '10.00', '1230.00', 0),
-(185, 11, 12, 10, 14, 41, 'Paulino', '2020-11-06 02:52:00', 'PPS21312', '2.00', '20.00', 0),
-(186, 11, 123, 10000, 14, 45, 'Paulina Robertina', '2020-11-06 04:11:00', 'PPPS123', '3.00', '30000.00', 0),
-(187, 11, 22, 100, 14, 40, '22', '2020-11-06 04:15:00', '2', '2.00', '200.00', 0),
-(188, 11, 21123, 490, 14, 41, 'Palunioa', '2020-11-06 04:25:00', 'PPS2', '2.00', '980.00', 0),
-(189, 11, 82782, 20000, 14, 46, 'Motoro', '2020-11-06 04:28:00', 'PPS23', '3.20', '64000.00', 0);
+(183, 11, 30, 10000.00, 14, 40, 'Luan Roberto Ribeiro', '2020-06-09 06:15:00', 'PPS2314', '3.20', '32000.00', 1),
+(184, 11, 21321, 123.00, 14, 40, 'thalles', '2020-06-09 11:04:00', 'PPS1232', '10.00', '1230.00', 1),
+(185, 11, 12, 10.00, 14, 41, 'Paulino', '2020-11-06 02:52:00', 'PPS21312', '2.00', '20.00', 1),
+(186, 11, 123, 10000.00, 14, 45, 'Paulina Robertina', '2020-11-06 04:11:00', 'PPPS123', '3.00', '30000.00', 1),
+(187, 11, 22, 100.00, 14, 40, '22', '2020-11-06 04:15:00', '2', '2.00', '200.00', 1),
+(188, 11, 21123, 490.00, 14, 41, 'Palunioa', '2020-11-06 04:25:00', 'PPS2', '2.00', '980.00', 1),
+(189, 11, 82782, 20000.00, 14, 46, 'Motoro', '2020-11-06 04:28:00', 'PPS23', '3.20', '64000.00', 1),
+(190, 11, 213, 100.00, 14, 48, 'Thalles', '2020-12-06 03:52:00', 'PPS232', '22.00', '2200.00', 1),
+(191, 11, 12, 1000.00, 14, 50, 'Humberto', '1970-01-01 01:00:00', 'PPP2342', '2.30', '2300.00', 1),
+(192, 11, 10000, 1.00, 14, 73, '0000', '1970-01-01 01:00:00', 'ppPP2', '1.20', '1.20', 1),
+(193, 11, 1000, 1.00, 14, 73, 'Talll', '1970-01-01 01:00:00', 'PPS2342', '2.02', '2.02', 1),
+(194, 11, 29388, 100.00, 14, 73, 'Paulo', '1970-01-01 01:00:00', 'ppps2', '2.22', '222.00', 1),
+(195, 11, 1223, 1000.00, 14, 74, 'Jonas', '1970-01-01 01:00:00', 'PPPS231', '3.20', '3200.00', 0);
 
 -- --------------------------------------------------------
 
@@ -481,7 +488,28 @@ CREATE TABLE `tbmov_saida` (
 --
 
 INSERT INTO `tbmov_saida` (`id_saida`, `id_cliente`, `id_veiculo`, `id_tanque`, `id_motorista`, `km`, `quantidade`, `data_hora`, `flag_excluido`) VALUES
-(51, 11, 42, 40, 41, 123, 100, '2020-06-11 00:00:00', 0);
+(51, 11, 42, 40, 41, 123, 100, '2020-06-11 00:00:00', 1),
+(52, 11, 42, 50, 41, 2500, 100, '2020-06-13 00:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tbmov_transito`
+--
+
+CREATE TABLE `tbmov_transito` (
+  `id_transito` int(11) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `id_fornecedor` int(11) NOT NULL,
+  `quantidade` double NOT NULL,
+  `data_hora` datetime NOT NULL,
+  `comprovante` varchar(220) NOT NULL,
+  `km` float NOT NULL,
+  `id_motorista` int(11) NOT NULL,
+  `id_combustivel` int(11) NOT NULL,
+  `id_veiculo` int(11) NOT NULL,
+  `flag_excluido` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -617,6 +645,13 @@ CREATE TABLE `tbsuporte` (
   `flag_excluido` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Despejando dados para a tabela `tbsuporte`
+--
+
+INSERT INTO `tbsuporte` (`id_suporte`, `id_cliente`, `requerente`, `titulo`, `descricao`, `arquivo`, `resposta`, `id_suporte_situacao`, `flag_excluido`) VALUES
+(9, 48, '48', '', 'Houve algo em algum lugar que eu preciso da sua ajuda..', NULL, NULL, 1, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -647,7 +682,8 @@ CREATE TABLE `tbtanque` (
   `id_tanque` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `nome_tanque` varchar(50) NOT NULL,
-  `capacidade` double NOT NULL,
+  `capacidade` double(10,2) NOT NULL,
+  `alerta_limite` double(10,2) NOT NULL DEFAULT 0.00,
   `id_combustivel` int(11) NOT NULL,
   `id_medida` int(11) NOT NULL,
   `flag_excluido` int(11) NOT NULL DEFAULT 0
@@ -657,15 +693,42 @@ CREATE TABLE `tbtanque` (
 -- Despejando dados para a tabela `tbtanque`
 --
 
-INSERT INTO `tbtanque` (`id_tanque`, `id_cliente`, `nome_tanque`, `capacidade`, `id_combustivel`, `id_medida`, `flag_excluido`) VALUES
-(40, 11, 'TAN. 01', 20000, 43, 4, 0),
-(41, 11, 'T-003', 1000, 45, 4, 0),
-(42, 11, 'BioD 02', 1000, 49, 4, 0),
-(43, 11, 'Dev H2', 11000, 43, 4, 0),
-(44, 11, 'p23', 2200, 45, 4, 0),
-(45, 11, 'GAS BR 2927 RI', 20000, 46, 4, 1),
-(46, 11, 'Petro Brasil 28236 Es - 9', 2000000, 50, 4, 1),
-(47, 11, 'T43', 20000, 45, 4, 1);
+INSERT INTO `tbtanque` (`id_tanque`, `id_cliente`, `nome_tanque`, `capacidade`, `alerta_limite`, `id_combustivel`, `id_medida`, `flag_excluido`) VALUES
+(40, 11, 'TAN. 01', 2000.02, 0.00, 43, 4, 1),
+(41, 11, 'T-003', 1000.00, 0.00, 45, 4, 1),
+(42, 11, 'BioD 02', 1000.00, 0.00, 49, 4, 1),
+(43, 11, 'Dev H2', 11000.00, 0.00, 43, 4, 1),
+(44, 11, 'p23', 2200.00, 0.00, 45, 4, 1),
+(45, 11, 'GAS BR 2927 RI', 20000.00, 0.00, 46, 4, 1),
+(46, 11, 'Petro Brasil 28236 Es - 9', 2000000.00, 0.00, 50, 4, 1),
+(47, 11, 'T43', 20000.00, 0.00, 45, 4, 1),
+(48, 11, 'T223', 1000.00, 0.00, 43, 4, 1),
+(49, 11, 'THAL 2', 20000.00, 30.00, 43, 4, 1),
+(50, 11, 'T-01', 20000.00, 30.00, 43, 4, 1),
+(51, 11, 'TT', 22.00, 22.00, 43, 13, 1),
+(52, 11, 'ttt', 222.00, 12.00, 43, 13, 1),
+(53, 11, 'tanque', 20000.00, 30.00, 43, 13, 1),
+(54, 11, 'TESTETAN', 100000.00, 30.00, 43, 4, 1),
+(55, 11, 'Tanque2q', 10000.00, 3030.00, 43, 4, 1),
+(56, 11, 'tanque', 10000.00, 2000.00, 43, 13, 1),
+(57, 11, 'T02-11', 10000.00, 20.00, 43, 4, 1),
+(58, 11, 'TTT', 20000.00, 30.00, 43, 4, 1),
+(59, 11, 't', 100000.00, 22.00, 43, 13, 1),
+(60, 11, 'tan teste 01', 200000.00, 30.00, 43, 4, 1),
+(61, 11, 'TANQ', 200000.00, 20.00, 43, 4, 1),
+(62, 11, 'TAN222', 100000.00, 20.00, 43, 4, 1),
+(63, 11, 'RRR3', 200000.00, 22.00, 43, 13, 1),
+(64, 11, 'OOOO', 2000.00, 20.00, 43, 13, 1),
+(65, 11, 'pppp', 1000.00, 20.00, 43, 4, 1),
+(66, 11, 'PPPP', 10000.00, 239.00, 43, 4, 1),
+(67, 11, 'RRRTEA', 2000.20, 20.00, 43, 8, 1),
+(68, 11, 'THALL', 1000.00, 30.00, 43, 4, 1),
+(69, 11, 'TAN', 2000.00, 2000.00, 43, 13, 1),
+(70, 11, 'TYY', 2000.00, 20.00, 43, 13, 1),
+(71, 11, 'KKKK', 2220.00, 20.00, 43, 13, 1),
+(72, 11, 'TANQ', 2000.00, 10000.00, 43, 5, 1),
+(73, 11, 'TTT', 200.00, 20.00, 43, 13, 0),
+(74, 11, 'OOOO', 2000.00, 12.22, 43, 4, 0);
 
 -- --------------------------------------------------------
 
@@ -684,6 +747,14 @@ CREATE TABLE `tbticket` (
   `id_fornecedor` int(11) NOT NULL,
   `flag_excluido` int(11) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Despejando dados para a tabela `tbticket`
+--
+
+INSERT INTO `tbticket` (`id_ticket`, `id_cliente`, `quantidade`, `data_entrada`, `id_veiculo`, `id_motorista`, `id_combustivel`, `id_fornecedor`, `flag_excluido`) VALUES
+(18, 11, 123, '2020-06-12', 42, 41, 43, 14, 1),
+(19, 11, 50, '2020-06-13', 42, 41, 45, 14, 0);
 
 -- --------------------------------------------------------
 
@@ -931,6 +1002,16 @@ ALTER TABLE `tbmov_saida`
   ADD KEY `id_motorista` (`id_motorista`);
 
 --
+-- Índices de tabela `tbmov_transito`
+--
+ALTER TABLE `tbmov_transito`
+  ADD PRIMARY KEY (`id_transito`),
+  ADD KEY `id_veiculo` (`id_veiculo`),
+  ADD KEY `id_combustivel` (`id_combustivel`),
+  ADD KEY `id_motorista` (`id_motorista`),
+  ADD KEY `id_fornecedor` (`id_fornecedor`);
+
+--
 -- Índices de tabela `tbnivel_acesso`
 --
 ALTER TABLE `tbnivel_acesso`
@@ -1029,7 +1110,7 @@ ALTER TABLE `tbabastecimento`
 -- AUTO_INCREMENT de tabela `tbcartao_virtual`
 --
 ALTER TABLE `tbcartao_virtual`
-  MODIFY `id_cartao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_cartao` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `tbcartao_virtual_movimentos`
@@ -1131,13 +1212,19 @@ ALTER TABLE `tbmotorista`
 -- AUTO_INCREMENT de tabela `tbmov_entrada`
 --
 ALTER TABLE `tbmov_entrada`
-  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=190;
+  MODIFY `id_entrada` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=196;
 
 --
 -- AUTO_INCREMENT de tabela `tbmov_saida`
 --
 ALTER TABLE `tbmov_saida`
-  MODIFY `id_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `id_saida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT de tabela `tbmov_transito`
+--
+ALTER TABLE `tbmov_transito`
+  MODIFY `id_transito` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `tbnivel_acesso`
@@ -1173,7 +1260,7 @@ ALTER TABLE `tbseguro`
 -- AUTO_INCREMENT de tabela `tbsuporte`
 --
 ALTER TABLE `tbsuporte`
-  MODIFY `id_suporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_suporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `tbsuporte_situacao`
@@ -1185,13 +1272,13 @@ ALTER TABLE `tbsuporte_situacao`
 -- AUTO_INCREMENT de tabela `tbtanque`
 --
 ALTER TABLE `tbtanque`
-  MODIFY `id_tanque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id_tanque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
 
 --
 -- AUTO_INCREMENT de tabela `tbticket`
 --
 ALTER TABLE `tbticket`
-  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_ticket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT de tabela `tbunidade_medida`
@@ -1291,6 +1378,15 @@ ALTER TABLE `tbmov_saida`
   ADD CONSTRAINT `tbmov_saida_ibfk_1` FOREIGN KEY (`id_veiculo`) REFERENCES `tbveiculo` (`id_veiculo`),
   ADD CONSTRAINT `tbmov_saida_ibfk_2` FOREIGN KEY (`id_tanque`) REFERENCES `tbtanque` (`id_tanque`),
   ADD CONSTRAINT `tbmov_saida_ibfk_3` FOREIGN KEY (`id_motorista`) REFERENCES `tbmotorista` (`id_motorista`);
+
+--
+-- Restrições para tabelas `tbmov_transito`
+--
+ALTER TABLE `tbmov_transito`
+  ADD CONSTRAINT `tbmov_transito_ibfk_1` FOREIGN KEY (`id_veiculo`) REFERENCES `tbveiculo` (`id_veiculo`),
+  ADD CONSTRAINT `tbmov_transito_ibfk_2` FOREIGN KEY (`id_combustivel`) REFERENCES `tbcategoria_combustivel` (`id_combustivel`),
+  ADD CONSTRAINT `tbmov_transito_ibfk_3` FOREIGN KEY (`id_motorista`) REFERENCES `tbmotorista` (`id_motorista`),
+  ADD CONSTRAINT `tbmov_transito_ibfk_4` FOREIGN KEY (`id_fornecedor`) REFERENCES `tbfornecedor` (`id_fornecedor`);
 
 --
 -- Restrições para tabelas `tbseguro`
